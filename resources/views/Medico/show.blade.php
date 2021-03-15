1512c64c8742e54e06dfd4999e7bc0f4b958f732
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-03-15 23:07:24 +0100

    Corretto l'if del genere
    Corretto l'if del genere

--- resources/views/Medico/show.blade.php
+++ resources/views/Medico/show.blade.php
@@ -9,33 +9,31 @@
                 <div class="left-show">
   
                     
-                    
-                    
-                    
-                    
-                    </div>
+                </div>
                     <div class="right-show">
                         <div class="container_top d-flex">
-                                <div class="container_dati">
-                                    <h3>@if(optional($medico->profile)->genere = 'maschio')
-                                        Dottor {{$medico->nome}} {{$medico->cognome}}
+                            <div class="container_dati">
+                                <h3>@if(optional($medico->profile)->genere === 'maschio')
+                                    Dottor {{$medico->nome}} {{$medico->cognome}}
                                         
-                                        @else
-                                        Dottoressa {{$medico->nome}} {{$medico->cognome}} 
+                                    @else
+                                    Dottoressa {{$medico->nome}} {{$medico->cognome}} 
                                         
-                                        @endif
-                                    </h3>
-                                    <div><strong>CITTA: </strong>{{optional($medico->profile)->città}}</div>
+                                    @endif
+                                </h3>
+                                <div><strong>CITT&Agrave;: </strong>{{optional($medico->profile)->città}}</div>
                                 <div><strong>INDIRIZZO: </strong>{{$medico->indirizzo}}</div>
                                 <div><strong>EMAIL: </strong>{{$medico->email}}</div>
                                 <div><strong>NUMERO TELEFONO: </strong>{{optional($medico->profile)->cellulare}}</div>
                                 <div><strong>P.IVA: </strong>{{optional($medico->profile)->piva}}</div>
-                                @if(optional($medico->profile)->disabilità = true)
-                                <strong>Accetto disabilità</strong>
-                                @else{
-                                    <strong>Non accetto disabilità</strong>
-                                }
-                                @endif
+                                <div class="disabilita_show">
+                                    @if(optional($medico->profile)->disabilità == true)
+                                    <strong>Accetto disabilità</strong>
+                                    @else{
+                                        <strong>Non accetto disabilità</strong>
+                                    }
+                                    @endif
+                                </div>
                                 
                             </div>
                             <div class="container_img d-flex">
