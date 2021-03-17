53ad7c6aeff791c675245ce32342f0c772e85581
Erjon <erjonlusha@gmail.com>
2021-03-17 10:38:13 +0100

    layout show medico2

--- resources/views/_partials/central/detail-medico.blade.php
+++ resources/views/_partials/central/detail-medico.blade.php
@@ -43,7 +43,29 @@ div
             <div class="container_bottom">
                 <div><strong>BIOGRAFIA: </strong>{!!optional($medico->profile)->bio!!}</div>                        
             </div>
-            <div class="bottom_right_bottom">
+            <div class="bottom_right_bottom form_message">
+                <h3>CONTATTA IL MEDICO</h3>
+                <form method="POST">
+                    <div class="form_input_message">                        
+                        <!-- nome -->
+                        <!-- <label for="nome_paziente"><strong>NOME</strong></label> -->
+                        <input id="nome_paziente" type="text" class="form-control" name="nome_paziente"  required autocomplete="nome_paziente" autofocus placeholder="Inserisci il tuo nome completo">
+                    </div>
+                    <div class="form_input_message">
+                        <!-- email -->
+                        <!-- <label for="email"><strong>EMAIL</strong></label> -->
+                        <input id="email" type="email" class="form-control" name="email" autocomplete="email" autofocus placeholder="Inserisci la tua email">
+                    </div>
+                    <div class="form_input_message">
+                        <!-- numero cellulare -->
+                        <!-- <label for="cellulare"><strong>NUMERO DI TELEFONO</strong></label> -->
+                        <input id="cellulare" type="text" class="form-control" name="cellulare"  required autocomplete="cellulare" autofocus placeholder="Inserisci un numero di telefono">
+                    </div>
+                    <!-- testo -->
+                    <!-- <label for="testo_messaggio"><strong>MESSAGGIO</strong></label> -->
+                    <textarea class="form-control" name="testo_messaggio" id="testo_messaggio" rows="3" placeholder="Inserisci il messaggio"></textarea>
+                </form>
+                <button type="button" class="btn">INVIO</button>
             </div>
         </div>
     </div>
