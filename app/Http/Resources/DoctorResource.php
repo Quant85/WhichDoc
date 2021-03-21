31fd9d175a3489abf8dcaf94845fb1146d2124da
Quant85 <antonio.85.q@gmail.com>
2021-03-21 23:23:55 +0100

    aggiornamento api

---
+++ app/Http/Resources/DoctorResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DoctorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'cognome' => $this->cognome,
+            'email' => $this->email,
+            'indirizzo' => $this->indirizzo,
+            'profilo' => $this->profile, 
+            'prestazioni' => PerformanceResource::collection($this->prestaziones),
+            'specializzazioni' => $this->Specializzaziones,
+            'recensioni' => $this->ratings,
+        ];
+    }
+
+    //public $preserveKeys = true;
+}
