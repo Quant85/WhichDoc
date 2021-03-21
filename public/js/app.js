8634786c32d3fdd1fea44827d4d797ca57f9f5b5
FraOliv <olivotti.francesco@gmail.com>
2021-03-21 21:36:16 +0100

    rest

--- public/js/app.js
+++ public/js/app.js
@@ -49870,50 +49870,20 @@ var app = new Vue({
   el: '#app'
 });
 
-function test() {
-  var tabsNewAnim = $('#navbarSupportedContent');
-  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-  var activeItemNewAnim = tabsNewAnim.find('.active');
-  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-  var itemPosNewAnimTop = activeItemNewAnim.position();
-  var itemPosNewAnimLeft = activeItemNewAnim.position();
-  $(".hori-selector").css({
-    "top": itemPosNewAnimTop.top + "px",
-    "left": itemPosNewAnimLeft.left + "px",
-    "height": activeWidthNewAnimHeight + "px",
-    "width": activeWidthNewAnimWidth + "px"
-  });
-  $("#navbarSupportedContent").on("click", "li", function (e) {
-    $('#navbarSupportedContent ul li').removeClass("active");
-    $(this).addClass('active');
-    var activeWidthNewAnimHeight = $(this).innerHeight();
-    var activeWidthNewAnimWidth = $(this).innerWidth();
-    var itemPosNewAnimTop = $(this).position();
-    var itemPosNewAnimLeft = $(this).position();
-    $(".hori-selector").css({
-      "top": itemPosNewAnimTop.top + "px",
-      "left": itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
-  });
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#category-img-tag').attr('src', e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
 }
 
-$(document).ready(function () {
-  setTimeout(function () {
-    test();
-  });
-});
-$(window).on('resize', function () {
-  setTimeout(function () {
-    test();
-  }, 500);
-});
-$(".navbar-toggler").click(function () {
-  setTimeout(function () {
-    test();
-  });
+$("#cat_image").change(function () {
+  readURL(this);
 });
 
 /***/ }),
