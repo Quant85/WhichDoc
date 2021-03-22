64c5dd3ac34f323cce3fe5a0d11fd6f6fefc49e7
Erjon <erjonlusha@gmail.com>
2021-03-22 10:25:24 +0100

    Modificato Carousel.vue
    Aggiunti link img commentato il v-for

--- resources/js/components/Carousel.vue
+++ resources/js/components/Carousel.vue
@@ -1,41 +1,61 @@
 <template>
   <div class="carousel-wrapper">
-    <VueSlickCarousel v-bind="slickOptions">
+    <VueSlickCarousel v-bind="settings">
       <!-- <div v-for="i in 5" :key="i" class="img-wrapper">
         <img :src="`./${i}-200x100.jpg`" />
       </div> -->
-      <div class="img-wrapper">
-        <img src="">
-        <img src="">
-        <img src="">
-        <img src="">
-        <img src="">
-      </div>
+    <div class="img-wrapper">
+        <img src="https://s3-eu-west-2.amazonaws.com/muralswallpaper/product_lifestyle_images/RO0012PI30W/1-elegant-pink-rose-wallpaper-mural-simple-bedroom.jpg">
+    </div>
+    <div class="img-wrapper">
+        <img src="https://www.muralswallpaper.com/app/uploads/elegant-pink-rose-flower-room-820x532.jpg">
+    </div>
+    <div class="img-wrapper">
+        <img src="https://www.muralswallpaper.com/app/uploads/Green-Tropical-Plant-Wallpaper-Mural-Room-820x532.jpg">
+    </div>
+    <div class="img-wrapper">
+        <img src="https://www.muralswallpaper.com/app/uploads/Jasmine-Bloomin-Marvellous-Wall-Mural-Room-KJ-820x532.jpg">
+    </div>
+    <div class="img-wrapper">
+        <img src="https://www.muralswallpaper.com/app/uploads/Fiesta-Blue-Chair-820x532.jpg">
+    </div>
     </VueSlickCarousel>
+    
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel'
+  // optional style for arrows & dots
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
 export default {
+    components: { VueSlickCarousel },
   data() {
     return {
-      slickOptions: {
-        slidesToShow: 3,
-        arrows: false
-      }
+     settings : {
+        "focusOnSelect": true,
+        "infinite": true,
+        "slidesToShow": 3,
+        "slidesToScroll": 1,
+        "speed": 500
+        }
+
     }
   }
 }
 </script>
 
 <style scoped >
-.carousel-wrapper {
-  padding: 40px;
-}
-.img-wrapper img {
-  margin: auto;
-  width: 200px;
-  height: 100px;
-  background-image: linear-gradient(gray 100%, transparent 0);
-}
+    .carousel-wrapper {
+        padding: 40px;
+    }
+    .img-wrapper img {
+        margin: auto;
+        width: 200px;
+        height: 100px;
+        background-image: linear-gradient(gray 100%, transparent 0);
+    }
 </style>
+
+
--- resources/views/guest/ricerca.blade.php
+++ resources/views/guest/ricerca.blade.php
@@ -1,5 +1,49 @@
 <div id="app">
-    <carousel-component>
-        
-    </carousel-component>
+    {{-- <carousel-component> --}}
+
+        <div id="container">
+          <div id="example"></div>
+        </div>
 </div>
+
+<script>
+    const buildSlideMarkup = (count) => {
+        let slideMarkup = '';
+    for (var i = 1; i <= count; i++) {
+        slideMarkup += '<slide><span class="label">' + i + '</span></slide>'
+    }
+    return slideMarkup;
+    };
+
+    new Vue({
+        el: '#example',
+    components: {
+        'carousel': VueCarousel.Carousel,
+        'slide': VueCarousel.Slide
+    },
+    template: '<div><carousel :navigationEnabled="true">' + buildSlideMarkup(10) + '</carousel></div>'
+    });
+</script>
+
+<style>
+    #container {
+  padding: 0 60px;
+}
+
+.VueCarousel-slide {
+  position: relative;
+  background: #42b983;
+  color: #fff;
+  font-family: Arial;
+  font-size: 24px;
+  text-align: center;
+  min-height: 100px;
+}
+
+.label {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
