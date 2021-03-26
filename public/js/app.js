3a40b9ced4629f17e3a484100d0e8d8c368e969e
Erjon <erjonlusha@gmail.com>
2021-03-26 17:42:31 +0100

    save

--- public/js/app.js
+++ public/js/app.js
@@ -63064,9 +63064,9 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/esercizi-boolean/whichdocofficial/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /var/www/html/esercizi-boolean/whichdocofficial/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /var/www/html/esercizi-boolean/whichdocofficial/resources/sass/guest/app.scss */"./resources/sass/guest/app.scss");
+__webpack_require__(/*! C:\Users\erion\Desktop\Boolean\Classe23\WhichDoc\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\erion\Desktop\Boolean\Classe23\WhichDoc\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\erion\Desktop\Boolean\Classe23\WhichDoc\resources\sass\guest\app.scss */"./resources/sass/guest/app.scss");
 
 
 /***/ })
