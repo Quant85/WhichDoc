beeb715e6cd9e025a9c90200fb827838bac705e7
NIcola R <nicola.rizzi89@hotmail.it>
2021-03-28 13:07:04 +0200

    modificato parte statistiche con disabilità

--- resources/views/guest/statistiche.blade.php
+++ resources/views/guest/statistiche.blade.php
@@ -1,3 +1,10 @@
-<div class="statistiche">
-    <H1>STATISTICHE</H1>
+<div class="statistiche row" >
+    <div class="statistiche-left col col-sm">
+       <h2>WhichDoc sostiene la disabilità</h2> <br>
+       <p>Abbiamo a cuore le vostre necessità</p>
+       <a type="button" class="btn btn-grad-secondary" href="">Clicca per saperne di più</a>
+    </div>
+    <div class="statistiche-right col col-sm">
+        <img src="{{asset('/img/disabilità2.jpg')}}" alt="">
+    </div>
 </div>
