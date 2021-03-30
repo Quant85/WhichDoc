31f1b6c23f4231f8a858f8e0638263fdc527fcb9
Quant85 <antonio.85.q@gmail.com>
2021-03-30 05:21:41 +0200

    Bio-Curriculare Edit

--- resources/views/Medico/profilo.blade.php
+++ resources/views/Medico/profilo.blade.php
@@ -26,8 +26,8 @@
                   </div>
                 @endif 
                 <h4 class="text-right">Profile Settings</h4>
-                <div class="">                        
-                  <div class="">
+                <div class="col-12 d-flex">                        
+                  <div class="col-6">
                     <label for="nome" class="labels">Nome</label>
                     <input type="text" name="nome" class="form-control" placeholder="first name" value="{{old('nome') ? old('nome') : $medico->nome}}">
                   </div>
@@ -35,7 +35,7 @@
                   <div class="alert alert-danger">{{ $message }}</div>
                   @enderror
 
-                  <div class="">
+                  <div class="col-6">
                     <label for="cognome" class="labels">Cognome</label>
                     <input type="text" name="cognome" class="form-control" value="{{old('cognome') ? old('cognome') : $medico->cognome}}" placeholder="surname">
                   </div>
@@ -44,48 +44,43 @@
                   @enderror
                 </div>
 
-                <div class="EMAIL">
-                  <div class="EMAIL">
+                <div class="col-12 d-flex">
+                  <div class="EMAIL col-4">
                     <label for="email" class="labels">Email</label>
                     <input type="text" name="email" class="form-control" value="{{old('email') ? old('email') : $medico->email}}" placeholder="email">
                   </div>
                   @error('email')
                   <div class="alert alert-danger">{{ $message }}</div>
                   @enderror
-                </div>
 
-                <div class="CELLULARE">
-                  <div class="CELLULARE">
+                  <div class="CELLULARE col-4">
                     <label for="cellulare"  class="labels">Contact Number</label>
                     <input type="text" name="cellulare" class="form-control" value="{{old('cellulare') ? old('cellulare') : optional($medico->profile)->cellulare}}" placeholder="number">
                   </div>
                   @error('cellulare')
                   <div class="alert alert-danger">{{ $message }}</div>
                   @enderror
+
+                    <div class="INDIRIZZO col-4">
+                      <label for="indirizzo" class="labels">Indirizzo</label>
+                      <input type="text" name="indirizzo" class="form-control" value="{{old('indirizzo') ? old('indirizzo')  : $medico->indirizzo}}" placeholder="address">
+                    </div>
+                    @error('indirizzo')
+                      <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                 </div>
 
-                <div class="INDIRIZZO">
-                  <div class="INDIRIZZO">
-                    <label for="indirizzo" class="labels">Indirizzo</label>
-                    <input type="text" name="indirizzo" class="form-control" value="{{old('indirizzo') ? old('indirizzo')  : $medico->indirizzo}}" placeholder="address">
-                  </div>
-                  @error('indirizzo')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                  @enderror
-                </div>
 
-                <div class="CITTA">
-                  <div class="CITTA">
+                <div class="col-12 d-flex">
+                  <div class="CITTA col-6">
                     <label for="città" class="labels">Città</label>
                     <input type="text" name="città" class="form-control" value="{{old('città') ? old('città') : optional($medico->profile)->città}} " placeholder="address">
                   </div>
                   @error('città')
                   <div class="alert alert-danger">{{ $message }}</div>
                   @enderror
-                </div>
 
-                <div class="PARTITA-IVA">
-                  <div class="PARTITA-IVA">
+                  <div class="PARTITA-IVA col-6">
                     <label for="piva" class="labels">PIVA</label>
                     <input type="text" name="piva" class="form-control" value="{{old('piva') ? old('piva') : optional($medico->profile)->piva}}" placeholder="address">
                   </div>
@@ -94,47 +89,45 @@
                   @enderror
                 </div>
 
-                <div class="GENERE">
-                  <h4>Genere</h4>
-                  <label for="genere" class="labels"><i class="fas fa-male"></i> Uomo</label>
-                  <input type="radio" class="form-control" value="maschio" name="genere" {{optional($medico->profile)->genere === 'maschio' ? 'checked' : ''}}>
-                  <label for="genere" class="labels"><i class="fas fa-female"></i> Donna </label>
-                  <input type="radio" id="donna" class="form-control" value="femmina" name="genere" {{optional($medico->profile)->genere === 'femmina' ? 'checked' : ''}}>
-                </div>
-                @error('genere')
-                  <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
-
-                <div class="DISABILITA">
-                  <label for="disabilità">Attenzione alle disabilità:</label>
-                  <input type="checkbox" name="disabilità" class="switch-input" value="1" {{ optional($medico->profile)->disabilità === 1 ? 'checked="checked"' : '' }}/>
-                </div>
-                @error('disabilità')
-                  <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
-
-
-
-                <div class="CV">
-                  <label for="cv"><i class="fas fa-file-pdf"style="font-size: 40px; color: blue; margin-top: 20px; cursor: pointer;" title="Carica il tuo cv"></i></label>
-                  <input type="file" class="form-control-file" name="cv" id="cv" placeholder="cerca" accept="{{-- image/*, --}}.pdf" aria-describedby="cvHelper" style="opacity: 0; position: absolute; z-index: -1;">
-                  <small id="cvHelper" class="form-text text-muted">Carica il tuo cv.pdf/img</small>
-                </div>
-                @error('cv')
-                  <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                    
-
-
-                <div class="form-group">
-                  <label for="bio">Bio:</label>
-                  <textarea id="bio" class="form-control " name="bio" cols="50" rows="10">{{old('bio') ? old('bio') : optional($medico->profile)->bio}}</textarea>
+                <div class="form-group col-12 py-3">
+                  <label for="bio">Bio-Curriculare</label>
+                  <textarea id="bio" class="ckeditor form-control " name="bio" cols="50" rows="10">{{old('bio') ? old('bio') : optional($medico->profile)->bio}}</textarea>
                 </div>
                 @error('bio')
                   <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
+
+                <div class="col-12 d-flex justify-content-around">
+                  <div class="GENERE">
+                    <h4>Genere</h4>
+                    <div class="d-flex">
+                      <label for="genere" class="labels"><i class="fas fa-male"></i> Uomo</label>
+                      <input type="radio" class="form-control" value="maschio" name="genere" {{optional($medico->profile)->genere === 'maschio' ? 'checked' : ''}}>
+                      <label for="genere" class="labels"><i class="fas fa-female"></i> Donna </label>
+                      <input type="radio" id="donna" class="form-control" value="femmina" name="genere" {{optional($medico->profile)->genere === 'femmina' ? 'checked' : ''}}>
+                    </div>
+                  </div>
+                  @error('genere')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                  @enderror
+
+                  <div class="CV">
+                    <label for="cv"><i class="fas fa-file-pdf"style="font-size: 40px; color: blue; margin-top: 20px; cursor: pointer;" title="Carica il tuo cv"></i></label>
+                    <input type="file" class="form-control-file" name="cv" id="cv" placeholder="cerca" accept="{{-- image/*, --}}.pdf" aria-describedby="cvHelper" style="opacity: 0; position: absolute; z-index: -1;">
+                    <small id="cvHelper" class="form-text text-muted">Carica il tuo cv.pdf/img</small>
+                  </div>
+                  @error('cv')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                  @enderror
+  
+                  <div class="DISABILITA">
+                    <label for="disabilità">Attenzione alle disabilità:</label>
+                    <input type="checkbox" name="disabilità" class="switch-input" value="1" {{ optional($medico->profile)->disabilità === 1 ? 'checked="checked"' : '' }}/>
+                  </div>
+                  @error('disabilità')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                  @enderror
+                </div>
 
                 <div class="col-md-12 d-flex justify-content-center align-items-center p-3">
                   <button type="submit" class="btn btn-primary d-flex justify-content-center"><i class="fas fa-sign-in-alt px-3"></i><i class="fas fa-user"></i></button>
@@ -142,10 +135,8 @@
               </div>
               <!-- <div class="bottom_left_footer"></div> -->
             </div>
-            
 
               <div class="bottom_right d-flex flex-column">
-
                 <div class="bottom_right_top">
 
                   <img src="{{optional($medico->profile)->foto ? asset( 'storage/'.$medico->profile->foto) : asset('img/default/dottori.jpg') }}" alt="foto profilo">
@@ -209,4 +200,10 @@
       <!-- /container bianco -->      
     </div>
   </div>
+    <script src="//cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
+  <script type="text/javascript">
+      $(document).ready(function() {
+        $('.ckeditor').ckeditor();
+      });
+  </script>
 @endsection
