962c78dd138dac5392122009015ec0f461918bfc
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-03-30 12:36:11 +0200

    Aggiunto stile login al register
    Aggiunto stile login al register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div id="register" class="container">
+        <div class="row justify-content-center">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -123,5 +122,5 @@
             </div>
         </div>
     </div>
-</div>
+
 @endsection
