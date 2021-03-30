d00d0bdf84cc271fcaedd0361e9312246f07d50f
Quant85 <antonio.85.q@gmail.com>
2021-03-30 18:07:53 +0200

    doctor message

--- resources/views/Message/index.blade.php
+++ resources/views/Message/index.blade.php
@@ -13,76 +13,76 @@
 
                 <div class="bottom d-flex flex-row">
                     <div  class="bottom_left d-flex justify-content-around flex-column">      
-                    <h1>Gestione Messaggi Ricevuti</h1>
-                    <div class="container">
-                        <table class="table table-striped">
-                            <thead >
-                                <tr>
-                                    <th scope="col">Mittente</th>
-                                    <th scope="col">Testo Messaggio</th>
-                                    <th scope="col">Email Mittente</th>
-                                    <th scope="col">Telefono Mittente</th>
-                                    <th scope="col">Ricevuto</th>
-                                    <th colspan = 2>Azioni</th>
-                                </tr>
-                            </thead>
-
+                        <h1>Gestione Messaggi Ricevuti</h1>
+                        <div class="container">
                             @foreach ($medico->messages as $message)
-                            <tbody>
-                            
-                                <tr>
-                                    <td>{{$message->nome_paziente}}</td>
-                                    <td>
-                                        <div class="form-group">
-                                            <label for=""></label>
-                                            <textarea class="form-control" name="" id="" rows="2" disabled>{{$message->testo_messaggio}}</textarea>
-                                        </div>
-                                        
-                                    </td>
-                                    <td>{{$message->email}}</td>
-                                    <td>{{$message->cellulare}}</td>
-                                    <td>{{\Carbon\Carbon::createFromTimeStamp(strtotime($message->created_at))->diffForHumans()}}
-                                    </td>
-                                    <td>
-                                        <a href="{{route('medico.messaggi.show', $message->id)}}" class="btn btn-info"><i class="far fa-eye"></i></a>
-                                    </td>
-                                    {{-- Botton trigger Modal --}}
-                                    <td>
-                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#destroy-{{ $message->id }}"><i class="fas fa-broom"></i></button>
-                                    </td>
-                                    {{-- Start Add Modal -  --}}
-                                    <div class="modal fade" id="destroy-{{ $message->id }}" tabindex="-1" role="dialog" aria-labelledby="message-destroy-{{ $message->id }}" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                            <h5 class="modal-title" id="#destroy-{{ $message->id }} title">Elimina Messaggio di {{$message->nome_paziente}}<i class="fas fa-broom"></i></h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
+                            <div class="messages_wrap my-4">
+                                <table class="table table-striped">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th scope="col">Mittente</th>
+                                            <th scope="col">Testo Messaggio</th>
+                                            <th scope="col">Email Mittente</th>
+                                            <th scope="col">Telefono Mittente</th>
+                                            <th scope="col">Ricevuto</th>
+                                            <th colspan = 2>Azioni</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    
+                                        <tr>
+                                            <td>{{$message->nome_paziente}}</td>
+                                            <td>
+                                                <div class="form-group">
+                                                    <label for=""></label>
+                                                    <textarea class="form-control"
+                                                                rows="1" disabled 
+                                                                style="border: 0px solid transparent">{{$message->testo_messaggio}}</textarea>
+                                                </div>
+                                            </td>
+                                            <td>{{$message->email}}</td>
+                                            <td>{{$message->cellulare}}</td>
+                                            <td>{{\Carbon\Carbon::createFromTimeStamp(strtotime($message->created_at))->diffForHumans()}}
+                                            </td>
+                                            <td>
+                                                <a href="{{route('medico.messaggi.show', $message->id)}}" class="btn btn-info"><i class="far fa-eye"></i></a>
+                                            </td>
+                                            {{-- Botton trigger Modal --}}
+                                            <td>
+                                                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#destroy-{{ $message->id }}"><i class="fas fa-trash-alt"></i></button>
+                                            </td>
+                                            {{-- Start Add Modal -  --}}
+                                            <div class="modal fade" id="destroy-{{ $message->id }}" tabindex="-1" role="dialog" aria-labelledby="message-destroy-{{ $message->id }}" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                    <h5 class="modal-title" id="#destroy-{{ $message->id }} title">Elimina Messaggio di {{$message->nome_paziente}}</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                    🚨 Sei sicuro di volerlo cancellare? 🚨 <br> 🚨 E se poi te ne penti?! 🚨
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-door-closed"></i></button>
+                                                    <form action="{{ route('medico.messaggi.destroy', $message->id)}}" method="post">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button class="btn btn-danger" type="submit"><i class="fas fa-bomb"></i></button>
+                                                    </form>
+                                                    </div>
+                                                </div>
+                                                </div>
                                             </div>
-                                            <div class="modal-body">
-                                            🚨 Sei sicuro di volerlo cancellare? 🚨 <br> 🚨 E se poi te ne penti?! 🚨
-                                            </div>
-                                            <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-door-closed"></i></button>
-                                            <form action="{{ route('medico.messaggi.destroy', $message->id)}}" method="post">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-danger" type="submit"><i class="fas fa-broom"></i></button>
-                                            </form>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </div>
-
-                                    {{-- End Add Model --}}
-                                </tr>
-                            </tbody>
+    
+                                            {{-- End Add Model --}}
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                             @endforeach
-                        
-                        </table>
-
-                    </div>
+                        </div>
                     </div>
                 </div>    
             </nav>      
